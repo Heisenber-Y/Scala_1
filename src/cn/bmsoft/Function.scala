46bdee2bcc8da767fb7defa3ffc16c9318eea6d2
menglongye <menglongye1@gmail.com>
2019-01-15 00:31:30 +0800

    commit 1

--- src/cn/bmsoft/Function.scala
+++ src/cn/bmsoft/Function.scala
@@ -5,14 +5,19 @@ object Function {
   def main(args: Array[String]): Unit = {
 
     // 默认参数和带名参数
-
     def decorate(str:String ,left:String = "[", right :String = "]")=left + str+right
-// left  和right 都有默认参数
+
+    //也可以在提供参数值的时候，指定参数名称 如：
+
+
+   print(decorate(left = "<<<",str = "hello",right = ">>>"))
+
+    // left  和right 都有默认参数
 //print(decorate("222"))
    //有默认参数但是使用自定义的参数
 //print(decorate("sss","<<<",">>>"))
    //如果给的参数不够的话，默认参数会从后往前逐个应用进来
- print(decorate("aaa","<<<"))
+ //print(decorate("aaa","<<<"))
 
 
   //定义函数
