2cd26dd3e49dfe5d4b19adbf44c0b9053a2ebf6c
menglongye <menglongye1@gmail.com>
2019-01-16 00:44:43 +0800

    11

---
+++ src/cn/bmsoft/Array.scala
@@ -0,0 +1,57 @@
+package cn.bmsoft
+
+import scala.collection.mutable.ArrayBuffer
+
+object Array {
+
+  def main(args: Array[String]): Unit = {
+
+  //变长数组，数组缓冲
+
+  // val b = ArrayBuffer[Int]()
+  //b +=1
+    //for(elem <- b) print(elem+"--")
+  //b += (1,2,3,4,5)
+   //for(elem <- b) print(elem+"---")
+   //b += (8,13,21)
+  // print("--------------")
+   // for(elem <- b ) println(elem+"----")
+  //移除最后5个元素
+    //b.trimEnd(5)
+//for (elem <- b) println(b+"---")
+
+
+  //在尾端添加多个元素，亿括号抱起来
+  // val ints = ArrayBuffer(1,2,3,4,5)
+  //for (elem <- ints) print(elem+"-")
+
+
+//一个空的数组，准备存放数据
+ // val b = ArrayBuffer[Int]()
+  //b +=1
+
+
+
+
+
+  //长度为2的Array[STRING] --类型是推断出来的
+    //说明：以提供初始值就不需要new
+    //val value = Array("Hello","world")
+
+
+
+    //10个元素的字符串数组，所有元素初始化为null
+ //  val strings = new Array[String](10)
+  //for (elem <- strings) print(elem+"--")
+
+
+
+  //val nums = new Array[Int](10)
+  //10个整数的数组，所有元素的初始化为0
+  //for (elem <- nums) print(elem+"---")
+
+
+  }
+
+
+}
--- src/cn/bmsoft/Function.scala
+++ src/cn/bmsoft/Function.scala
@@ -9,35 +9,35 @@ object Function {
 
 
   //可以使用任意多的参数来调用改函数，函数得到的式Seq 函数
-def  sum(args:Int*)={
-
-var result =0
-  for(arg <- args) result += arg
-
-result
-
-}
+//def  sum(args:Int*)={
+//
+//var result =0
+//  for(arg <- args) result += arg
+//
+//result
+//
+//}
 // val i = sum(1,2,3,4,5)
 
  //如果sum函数被调用式传入的是单个参数，则参数必须是单个整数
     //而不是一个区间，解决办法：追加 _* 即可
-    val i = sum(1 to 5:_*)
+//    val i = sum(1 to 5:_*)
 
  //常见使用案例
-
-   def  recursiveSum(args: Int*): Int={
-
-if(args.length==0) 0
-      else args.head + recursiveSum(args.tail:_*)
-
-    }
- print(recursiveSum(1,2,3))
+//
+//   def  recursiveSum(args: Int*): Int={
+//
+//if(args.length==0) 0
+//      else args.head + recursiveSum(args.tail:_*)
+//
+//    }
+// print(recursiveSum(1,2,3))
 
 
     // 错误示范： 如果已经有一个值的序列，则不能直接将他传入上述参数
    // val i = sum(1 to 5)
 
-print(i)
+//print(i)
 
 
 
@@ -45,7 +45,7 @@ print(i)
 
 
     // 默认参数和带名参数
-    def decorate(str:String ,left:String = "[", right :String = "]")=left + str+right
+   // def decorate(str:String ,left:String = "[", right :String = "]")=left + str+right
 
     //也可以在提供参数值的时候，指定参数名称,注意 带参数名并不需要跟参数列表的顺序完全一直 如：
 
