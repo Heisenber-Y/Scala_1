9708468807036c26c648fd4a7fd6de2e1dabbdfb
menglongye <menglongye1@gmail.com>
2019-01-16 00:53:43 +0800

    111

--- src/cn/bmsoft/Array.scala
+++ src/cn/bmsoft/Array.scala
@@ -7,9 +7,24 @@ object Array {
   def main(args: Array[String]): Unit = {
 
   //变长数组，数组缓冲
+    val strings = ArrayBuffer[String]()
+    strings  +=("1","2","3")
+    strings.insert(2,"a","b","c")
+    //for (elem<- strings) print(elem)
+    //第一个参数表示插入或者删除的位置。第二个参数表示需要移除多少个元素
+    strings.remove(2,3)
+    for(elem <- strings) print(elem)
 
-  // val b = ArrayBuffer[Int]()
-  //b +=1
+
+    //val b = ArrayBuffer[Int]()
+ // b += (1,2,3,4,5)
+
+    //b.insert(2,9)
+    //for (elem <- b) print(elem+"-")
+
+
+
+    //b +=1
     //for(elem <- b) print(elem+"--")
   //b += (1,2,3,4,5)
    //for(elem <- b) print(elem+"---")
