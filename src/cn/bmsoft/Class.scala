dd77350bf07baf47f5b88dcffddeb738b885633d
menglongye <menglongye1@gmail.com>
2019-05-05 19:16:24 +0800

    第er次提交测试

--- src/cn/bmsoft/Class.scala
+++ src/cn/bmsoft/Class.scala
@@ -2,8 +2,14 @@ package cn.bmsoft
 
 object Class {
     //简单类和无参方法：
+  private var value=0
+  def increment (){value +=1}
+  def current()=value
 
-
+//一个类如果没有显示定义主构造器，则自动拥有一个无参的构造器即可，
+  //val p1= new Person();  主构造器
+  //val p2=new Person("Fred") 第一个辅助构造器
+  //val p3=new Person("Fred",22)  第二个辅助构造器
 
 
 }
